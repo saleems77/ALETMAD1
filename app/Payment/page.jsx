2011222d41289d9c6cb4812eb21f644972eca355
Mohammed Nour <mohammednourteeby@gmail.com>
2025-07-09 17:52:24 +0300

    delete

--- app/Payment/page.jsx
+++ app/Payment/page.jsx
@@ -1,15 +1,6 @@
-'use client';
-
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-import PaymentMethodSelector from './PaymentMethodSelector';
-
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 export default function PaymentPage() {
   return (
-    <Elements stripe={stripePromise}>
-      <PaymentMethodSelector onPaymentMethodSelected={(pm) => console.log(pm)} />
-    </Elements>
+    <div> Payment</div>
   );
 }
